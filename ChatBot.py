ce2e6fb51f35d0d9df2dc846436fa429dcdf844f
Vinodraj VK <tecraj19@gmail.com>
2023-12-26 23:17:37 +0530

    Initial Commit

---
+++ ChatBot.py
@@ -0,0 +1,74 @@
+import json
+import difflib
+import random
+import logging
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Configure Logging
+logging.basicConfig(
+    level=logging.INFO,
+    filename='Campus_Companion.log',
+    filemode='w',
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(__name__)
+
+# Load The JSON File
+# Define the filename in a configuration section
+INTENTS_JSON_FILE = "intents_with_variations.json"
+
+# Load The JSON File
+with open(INTENTS_JSON_FILE, "r") as file:
+    intents_data = json.load(file)
+
+
+# Predict The Intent
+
+
+def get_intent(user_input):
+    matches = difflib.get_close_matches(user_input.lower(), [
+        pattern for intent in intents_data["intents"] for pattern in intent["patterns"]], n=1, cutoff=0.6)
+
+    if matches:
+        matched_intent = next(
+            intent for intent in intents_data["intents"] if matches[0] in intent["patterns"])
+        return matched_intent
+    else:
+        return None
+
+# Generate The Response based on Intent
+
+
+def get_response(intent):
+    responses = intent["responses"]
+    return random.choice(responses)
+
+# Flask route for handling chatbot requests
+
+
+@app.route('/chatbot', methods=['POST', 'GET'])
+def chatbot():
+    data = request.get_json()
+    user_input = data.get('user_message', '')
+    logger.info("User : %s", user_input)
+
+    if user_input.lower() == 'exit':
+        logger.info("User requested exit.")
+        return jsonify({"response": "Goodbye!"})
+
+    matched_intent = get_intent(user_input)
+
+    if matched_intent:
+        response = get_response(matched_intent)
+        logger.info("Response : %s", response)
+        return jsonify({"response": response})
+    else:
+        logger.warning("Unrecognized user input: %s", user_input)
+        return jsonify({"response": "Sorry, I don't understand that. Can you please rephrase?"})
+
+
+if __name__ == '__main__':
+    logger.info("Bot: Hi! How can I assist you today?")
+    app.run(host='0.0.0.0', port=5000)
---
+++ VariationsGenerator.py
@@ -0,0 +1,60 @@
+import json
+import random
+from nltk.tokenize import word_tokenize
+
+
+def generate_sentence_variations(sentence, num_variations=5):
+    variations = []
+
+    # Tokenize the sentence into words
+    words = word_tokenize(sentence)
+
+    for _ in range(num_variations):
+        # Shuffle the words to create a variation
+        random.shuffle(words)
+        new_variation = ' '.join(words)
+        variations.append(new_variation)
+
+    return variations
+
+
+def generate_variations_for_intents(intents_data, num_variations=3):
+    new_intents_data = {"intents": []}
+
+    for intent in intents_data["intents"]:
+        new_patterns = []
+
+        for pattern in intent["patterns"]:
+            variations = generate_sentence_variations(pattern, num_variations)
+            new_patterns.extend(variations)
+
+        # Include the original patterns along with their variations
+        new_patterns.extend(intent["patterns"])
+
+        # Remove duplicates
+        new_patterns = list(set(new_patterns))
+        if 'responses' in intent:
+            new_intent = {
+                "tag": intent["tag"],
+                "patterns": new_patterns,
+                "responses": intent["responses"]
+            }
+
+        new_intents_data["intents"].append(new_intent)
+
+    return new_intents_data
+
+
+# Load intents from the original file
+with open("intents.json", "r") as file:
+    original_intents_data = json.load(file)
+
+# Generate variations for each sentence in the patterns
+new_intents_data = generate_variations_for_intents(
+    original_intents_data, num_variations=5)
+
+# Save the new intents data to a new file
+with open("intents_with_variations.json", "w") as file:
+    json.dump(new_intents_data, file, indent=2)
+
+print("Variations Generated and saved to intents_with_variations.json.")
